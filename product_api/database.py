7f9110b0d8ff92f2237a6cf1eec2117b9805fada
utkarsh0657 <utkarshsrivastava1000@gmail.com>
2024-06-02 20:15:54 +0530

    db_changes

--- product_api/database.py
+++ product_api/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# import os
+import os
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Bighead123@localhost:5432/product"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
